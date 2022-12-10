4da4122772610abc107db31c3e963fcc9cba7be3
Ivio Feng <115487784+ivios@users.noreply.github.com>
2022-12-10 16:31:03 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,9 @@
-
+float grandeValore (float array[], int n){
+    float Valore = 0;
+    for (int i=0; i<n; i++){
+        if(array[i]>Valore){
+            Valore = array[i];
+        }
+    }
+    return Valore;
+}
