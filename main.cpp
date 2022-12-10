0934e62c3b7429d1de5d03d43dbe56017c766fdf
Ivio Feng <115487784+ivios@users.noreply.github.com>
2022-12-10 16:30:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int n;
+    cin >> n;
+    float valori[n];
+    for (int i=0; i<n; i++){
+        cin >> valori[i];
+    }
+    cout << grandeValore(valori,n);
+    return 0;
 }
